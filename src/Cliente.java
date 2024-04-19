02332b5a7f783781594513dec7f10d9cadbbb56d
jmpl10 <jmpl@educa.madrid.org>
2024-04-19 12:21:32 +0200

    version 2

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,11 +1,11 @@
-package org.example;
-
-
 public class Cliente {
     private String nombre;
     private int edad;
     private Vehiculo [] vehiculos= new Vehiculo[2];
     void anadir(Vehiculo v){
         vehiculos[0]=v;
+    }
+    Cliente(String nombre){
+        this.nombre=nombre;
     }
 }
